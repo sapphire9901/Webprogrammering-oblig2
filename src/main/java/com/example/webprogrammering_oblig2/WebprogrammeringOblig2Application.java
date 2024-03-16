80ee64b4086076788fd59810e0f6c339ec1ef1e4
sapphire9901 <sagro5021@oslomet.no>
2024-03-16 20:47:09 +0100

    oppretter sprigboot og endret på readme

---
+++ src/main/java/com/example/webprogrammering_oblig2/WebprogrammeringOblig2Application.java
@@ -0,0 +1,13 @@
+package com.example.webprogrammering_oblig2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprogrammeringOblig2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebprogrammeringOblig2Application.class, args);
+    }
+
+}
